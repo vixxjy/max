539d0c0f352633c892552210221aa095b46f6427
vixxjy <ajorvictor48@gmail.com>
2019-06-14 10:37:12 +0100

    go questions solution

---
+++ src/question-5.go
@@ -0,0 +1,47 @@
+package models
+
+import (
+    "database/sql"
+    "time"
+
+    _ "github.com/lib/pq"
+)
+
+type Tasks struct {
+    ID int
+    Title string
+    Done bool
+    UserID int
+    CreatedAt time.Time
+}
+
+var Db * sql.DB
+
+func main() {
+    var err error
+
+    Db, err = sql.Open("postgres", "user=postgres dbname=go_app sslmode=disable")
+    if err != nil {
+        panic(err)
+    }
+}
+
+func(task * Tasks) Create() error {
+    query: = "insert into tasks (title, user_id) values ($1, $2) returning id, done"
+
+        stmt,
+    err: = Db.Prepare(query)
+
+    if err != nil {
+        panic(err)
+    }
+
+    stmt.Close()
+    err = stmt.QueryRow(task.Title, task.User_id)
+    .Scan( & task.Id, & task.Done)
+
+    if err != nil {
+        return err
+    }
+    return
+}
